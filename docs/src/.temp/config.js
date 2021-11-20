c9eb372ec8ce16fd16713d4e845a8a6241af922d
Charlie Haley <charlie.haley@hotmail.com>
2021-11-20 19:09:13 +0000

    chore: tidy files

--- docs/src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - flowcontrol",
-  "siteUrl": "https://flowctl.app",
-  "version": "0.7.13",
-  "catchLinks": true
-}
--- docs/src/.temp/now.js
+++
@@ -1,1 +0,0 @@
-export default 1637433782046
